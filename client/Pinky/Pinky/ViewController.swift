efcc83dc8b46c376b2a42d7c5b7cd3327dc29e4d
takuyamakawa <g2117052@fun.ac.jp>
2018-10-26 18:26:13 +0900

    create Xcode Proj

---
+++ client/Pinky/Pinky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pinky
+//
+//  Created by 山川拓也 on 2018/10/26.
+//  Copyright © 2018 山川拓也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
