0e4d1e5afa2dc83a8c26e63f57412ff9c9b7354c
YutoKumagai <b1013130@fun.ac.jp>
2018-10-27 13:22:27 +0900

    加速度を取得する関数を作成

--- device/app.py
+++ device/app.py
@@ -5,13 +5,27 @@ from time import sleep
 import RPi.GPIO as GPIO
 
 MICPIN = 4
+SPICS = 8
+SPIMISO = 9
+SPIMOSI = 10
+SPICLK = 11
 LEDPIN = 17
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(MICPIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(SPICLK, GPIO.OUT)
+GPIO.setup(SPIMOSI, GPIO.OUT)
+GPIO.setup(SPIMISO, GPIO.IN)
+GPIO.setup(SPICS, GPIO.OUT)
+GPIO.setup(LEDPIN, GPIO.OUT)
 GPIO.setup(LEDPIN, GPIO.OUT)
 
 status = 'WAIT'
+numOfTouch = 0
+touchLogList = []
+word = ''
+axisVal = [0.0, 0.0, 0.0]
+axisPrevVal = [0.0, 0.0, 0.0]
 
 def record():
   global status
@@ -56,6 +70,45 @@ def record():
   
   status = 'WAIT'
 
+
+def readadc(adcnum):
+    global SPICS
+    global SPIMISO
+    global SPIMOSI
+    global SPICLK
+
+    if adcnum > 7 or adcnum < 0:
+        return -1
+
+    GPIO.output(SPICS, GPIO.HIGH)
+    GPIO.output(SPICLK, GPIO.LOW)
+    GPIO.output(SPICS, GPIO.LOW)
+
+    commandout = adcnum
+    commandout |= 0x18  # スタートビット＋シングルエンドビット
+    commandout <<= 3    # LSBから8ビット目を送信するようにする
+
+    for i in range(5):
+        # LSBから数えて8ビット目から4ビット目までを送信
+        if commandout & 0x80:
+            GPIO.output(SPIMOSI, GPIO.HIGH)
+        else:
+            GPIO.output(SPIMOSI, GPIO.LOW)
+        commandout <<= 1
+        GPIO.output(SPICLK, GPIO.HIGH)
+        GPIO.output(SPICLK, GPIO.LOW)
+    adcout = 0
+    # 13ビット読む（ヌルビット＋12ビットデータ）
+    for i in range(13):
+        GPIO.output(SPICLK, GPIO.HIGH)
+        GPIO.output(SPICLK, GPIO.LOW)
+        adcout <<= 1
+        if i>0 and GPIO.input(SPIMISO)==GPIO.HIGH:
+            adcout |= 0x1
+    GPIO.output(SPICS, GPIO.HIGH)
+    return adcout  
+
+  
 def micCallback(channnel):
   record()
 
