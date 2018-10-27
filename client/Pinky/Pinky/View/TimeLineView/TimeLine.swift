b98a6fefa919cb6851bc0be206cc6a6e42d03195
takuyamakawa <g2117052@fun.ac.jp>
2018-10-27 11:57:58 +0900

    insert logo in header

--- client/Pinky/Pinky/View/TimeLineView/TimeLine.swift
+++ client/Pinky/Pinky/View/TimeLineView/TimeLine.swift
@@ -19,21 +19,22 @@ class TimeLine: UIViewController, UITableViewDelegate, UITableViewDataSource{
         
         self.timeline.delegate = self
         self.timeline.dataSource = self
-//        self.timeline.register(UINib(nibName: "TimeLineCell", bundle: nil), forCellReuseIdentifier: "TimeLineCell")
+
     }
     
-    // MARK: UITableViewDataSource
-    // セクション数
+    override func viewWillAppear(_ animated: Bool) {
+        let titleview = UIImageView(image: UIImage(named: "pinky_header"))
+        self.navigationItem.titleView = titleview
+    }
+
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
-    // セル数
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.hoge.count
     }
     
-    // セル内容
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TimeLineCell") as! TimeLineCell
         
@@ -42,34 +43,9 @@ class TimeLine: UIViewController, UITableViewDelegate, UITableViewDataSource{
         return cell
     }
     
-    // MARK: UITableViewDelegate
-    // セルタップ時の処理
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         // タップ時の処理を記述
         
     }
 }
-
-//extension TimeLine: UITableViewDelegate, UITableViewDataSource {
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//
-//        return hoge.count
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "TimeLineCell", for: indexPath) as! TimeLineCell
-//
-//        return cell
-//    }
-//
-////    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-////        return UIScreen.main.bounds.height / 8.0
-////    }
-//
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print(indexPath.row)
-//    }
-//}
-
-
