29ba8bf828ad7cf7e86edde48c7bc80e4558f315
takuyamakawa <g2117052@fun.ac.jp>
2018-10-27 13:33:27 +0900

    create model

---
+++ client/Pinky/Pinky/struct.swift
@@ -0,0 +1,20 @@
+//
+//  struct.swift
+//  Pinky
+//
+//  Created by 山川拓也 on 2018/10/27.
+//  Copyright © 2018 山川拓也. All rights reserved.
+//
+
+struct Promise : Codable{
+    let content : String
+    let created_at : String
+    let img : String
+    let is_master : Bool
+    let limited_at : String
+    let name : String
+}
+
+struct Promises : Codable{
+    let promises : [Promise]
+}
