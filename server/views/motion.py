3453ab78978ebe336abd292e6c475d42ae8a4603
Kenta Iwami <g2117009@fun.ac.jp>
2018-10-27 17:49:00 +0900

    ・promiseAPIにカレンダー追加を楽にするために、パラメータを追加した ・promiseAPIの返り順を作成順にした ・motionはテスト中

--- server/views/motion.py
+++ server/views/motion.py
@@ -1,16 +1,33 @@
 from flask import Blueprint, jsonify, request
-import requests
 from database import session
-from model import User, Promise
+from model import User, Promise, Motion
 import os
-import uuid
 from datetime import datetime as DT
-
+import datetime
 
 app = Blueprint('motion_bp', __name__)
 
 
 @app.route('/motion', methods=['POST'])
 def post():
+
+    # マッチング判定
+    # モーションテーブルから近い時刻のレコードがあるか検索。
+    # なかったら、モーションテーブルに時刻を追加。
+    start = DT.now()
+
+    print('******************:')
+    print(start)
+    print(start - datetime.timedelta(seconds=5))
+    print('******************:')
+
+    # motion = session.query(Motion).filter(Motion.created_at.between())
+
+    ### 約束IDあり
+    # あったら、そのレコードにあるユーザIDを持ってきて約束テーブルの該当レコードに追記。
+    #
+    # ### 約束IDなし
+    # あったら、そのレコードにあるユーザIDと約束IDを持ってきて、約束テーブルの該当レコードに追記。
+
 
     return jsonify({'results': 'OK'}), 200
--- server/views/promise.py
+++ server/views/promise.py
@@ -16,17 +16,20 @@ def get(user_id):
         or_(User.id == Promise.master_user_id, User.id == Promise.slave_user_id),
         User.id != user_id,
         Promise.is_done == False
-    ).all()
+    ).order_by(Promise.created_at.desc()).all()
 
     results = []
     for promise, user in promises:
         results.append({
             'created_at': promise.created_at.strftime('%Y年%m月%d日'),
             'limited_at': '' if promise.limit_date is None else promise.limit_date.strftime('%Y年%m月%d日'),
+            'calendar_date': '' if promise.limit_date is None else promise.limit_date.strftime('%Y/%m/%d %H:%M:%S'),
             'img': '{}/{}'.format(HOST_TOP, user.profile),
             'name': user.name,
             'content': promise.content,
             'is_master': True if str(promise.master_user_id) == str(user_id) else False,
         })
+
+    session.close()
 
     return jsonify({'results': results})
