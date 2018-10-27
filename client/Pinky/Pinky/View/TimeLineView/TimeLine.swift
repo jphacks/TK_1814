8c6d6981a3c2a889436294c33d2e1e98deb15a28
takuyamakawa <g2117052@fun.ac.jp>
2018-10-27 11:27:30 +0900

    tableviewに素材設置

--- client/Pinky/Pinky/View/TimeLineView/TimeLine.swift
+++ client/Pinky/Pinky/View/TimeLineView/TimeLine.swift
@@ -8,12 +8,68 @@
 
 import UIKit
 
-class TimeLine: UIViewController {
+class TimeLine: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    @IBOutlet weak var timeline: UITableView!
+    
+    let hoge = ["ほげ","Hello,World!","testなんやで"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.timeline.delegate = self
+        self.timeline.dataSource = self
+//        self.timeline.register(UINib(nibName: "TimeLineCell", bundle: nil), forCellReuseIdentifier: "TimeLineCell")
     }
     
+    // MARK: UITableViewDataSource
+    // セクション数
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // セル数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.hoge.count
+    }
+    
+    // セル内容
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TimeLineCell") as! TimeLineCell
+        
+        cell.profileName.text = String(indexPath.row + 1)
+        cell.promise.text = self.hoge[indexPath.row]
+        return cell
+    }
+    
+    // MARK: UITableViewDelegate
+    // セルタップ時の処理
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // タップ時の処理を記述
+        
+    }
 }
+
+//extension TimeLine: UITableViewDelegate, UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//
+//        return hoge.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "TimeLineCell", for: indexPath) as! TimeLineCell
+//
+//        return cell
+//    }
+//
+////    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+////        return UIScreen.main.bounds.height / 8.0
+////    }
+//
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(indexPath.row)
+//    }
+//}
+
 
