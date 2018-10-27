d2264e028cb122c9b69ceb054dd97891979a0b59
YutoKumagai <b1013130@fun.ac.jp>
2018-10-27 12:10:22 +0900

    録音をする処理を追加

--- device/app.py
+++ device/app.py
@@ -14,8 +14,42 @@ def record():
   global status
 
   status = 'RECORD'
-  print("hoge")
-  sleep(1)
+
+  CHUNK = 1024
+  FORMAT = pyaudio.paInt16
+  CHANNELS = 1
+  RATE = 48000
+  RECORD_SECONDS = 5
+  WAVE_OUTPUT_FILENAME = "output.wav"
+
+  p = pyaudio.PyAudio()
+
+  stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+  print("* recording")
+
+  frames = []
+
+  for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+      data = stream.read(CHUNK, exception_on_overflow = False)
+      buf = np.frombuffer(data, dtype="int16") # 読み込んだストリームデータを2byteのInt型のリストに分離
+
+  print("* done recording")
+
+  stream.stop_stream()
+  stream.close()
+  p.terminate()
+
+  wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+  wf.setnchannels(CHANNELS)
+  wf.setsampwidth(p.get_sample_size(FORMAT))
+  wf.setframerate(RATE / 3) # ヘッダのサンプリングレートを16kHzにする
+  wf.writeframes(b''.join(frames))
+  wf.close()
   
   status = 'WAIT'
 
